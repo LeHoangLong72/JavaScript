0605fdfe6811b30ffba2df1a7d43eb164d5e38b8
Le Hoang Long <nganlang9377@gmail.com>
2025-03-27 21:19:38 +0700

    Bai4

---
+++ Bai4/main.js
@@ -0,0 +1,34 @@
+let myString = `abc`;
+console.log(myString);
+
+// kiểm tra kiểu dữ liệu của biến myString
+console.log(typeof myString);
+
+// kiểu number
+let soNguyen = 10;
+let soThuc = 3.14;
+
+// xuất giá trị của biến
+console.log(soNguyen);
+console.log(soThuc);
+console.log(typeof soNguyen);
+console.log(typeof soThuc);
+
+// kiểu boolean
+let check = false;
+console.log(typeof check);
+
+// kiểu undefined
+
+let diemToan;
+
+console.log(typeof diemToan);
+
+
+// kiểu null
+let connect = null;
+console.log(typeof connect);
+
+// xuất số nguyên an toàn tối đa/ tối thiểu
+console.log("Số nguyên an toàn tối đa: " + Number.MAX_SAFE_INTEGER);
+console.log("Số nguyên an toàn tối thiểu: " , Number.MIN_SAFE_INTEGER);
