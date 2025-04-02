f1550d57c845a4144594c1ad334f92eb8857d895
Le Hoang Long <se182662lehoanglong@gmail.com>
2025-04-02 15:46:29 +0700

    Bai5_3

---
+++ Bai5_3/main.js
@@ -0,0 +1,14 @@
+// Ưu tiên ()
+let diemToan = 9.5;
+let diemVan = 8.75;
+
+let dtb = (diemToan + diemVan) / 2;
+console.log(dtb);
+
+// right to left
+x = y = 25;
+
+// left to right
+let c = 25 - 7 + 8 - 1;
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence
