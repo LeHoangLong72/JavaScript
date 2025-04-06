97b3506d6a8b81cdd8fdfae9854c980d14c8366b
Lê Hoàng Long <se182662lehoanglong@gmail.com>
2025-04-06 20:20:47 +0700

    Bài tập 7

---
+++ Baitap7/main.js
@@ -0,0 +1,20 @@
+let a = 7;
+a += 3;
+console.log(a);
+
+let b = 15;
+b -= 6;
+console.log(b);
+
+let c = 5;
+c *= 4;
+console.log(c);
+
+let d = 12;
+d /= 2;
+console.log(d);
+
+let e = 5,
+  f = 2;
+e -= f + 1;
+console.log(e);
