a04de7001facd0136786769209ff67d2284b0dbc
Lê Hoàng Long <se182662lehoanglong@gmail.com>
2025-04-07 10:17:19 +0700

    Bài tập 1

---
+++ Baitap9_1/main.js
@@ -0,0 +1,8 @@
+let r = Number(prompt("Nhập bán kính của đường tròn:"));
+let PI = Math.PI;
+
+let chuvi = 2 * PI * r;
+let dientich = PI * r * r;
+
+console.log(`Chu vi = ${chuvi}`);
+console.log(`Diện tích = ${dientich}`);
