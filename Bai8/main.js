b7989ae7fd4a766bc9eb208b8bbe832794780683
Lê Hoàng Long <se182662lehoanglong@gmail.com>
2025-04-07 09:52:04 +0700

    Bai8

---
+++ Bai8/main.js
@@ -0,0 +1,26 @@
+// Toán Tử Tăng/Giảm: Toán tử tiền tố hậu tố (++, --)
+// Dùng để tăng hoặc giảm giá trị của biến 1 đơn vị.
+
+let a = 99;
+let b = 10;
+let c = 77;
+let d = 88;
+a++;
+b--;
+++c;
+--d;
+
+// xuất kết quả
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+// Trường hợp biểu thức phức tạp
+let x = 1;
+let y = 2;
+let z = x++ - ++y + 1;
+
+console.log(x);
+console.log(y);
+console.log(z);
