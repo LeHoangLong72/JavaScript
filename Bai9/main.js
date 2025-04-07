289ff10d9c57f61c988dea5900a046597de51af9
Lê Hoàng Long <se182662lehoanglong@gmail.com>
2025-04-07 10:08:33 +0700

    Bai9

---
+++ Bai9/main.js
@@ -0,0 +1,14 @@
+/// toán tử logic
+let i = 7;
+// kiểm tra xem i > 0 và i < 10 không?
+console.log(i > 0 && i < 10);
+console.log(i > 0);
+console.log(i < 10);
+
+// kiểm tra xem i > 0 hoặc i < 10 không?
+console.log(i > 0 || i < 10);
+console.log(i > 0);
+console.log(i < 10);
+
+// toán tử đảo ngược !
+console.log(!(i < 10));
