bfae45fb9e84b9fe8f61b3455f53e9e336e015b1
Lê Hoàng Long <se182662lehoanglong@gmail.com>
2025-04-07 10:29:20 +0700

    Bài tập 2

---
+++ Baitap9_2/main.js
@@ -0,0 +1,8 @@
+let a = Number(prompt("Nhập chiều dài của hình chữ nhật:"));
+let b = Number(prompt("Nhập chiều rộng của hình chữ nhật:"));
+
+let s = a * b;
+let p = (a + b) * 2;
+
+console.log(`Chu vi = %s`, p);
+console.log(`Diện tích = %s`, s);
