763c6e5f8022fc9d6d4abae3eff3ab9a50c03f36
LeHoangLong72 <se182662lehoanglong@gmail.com>
2025-04-10 20:51:59 +0700

    Bai15

---
+++ Bai15/main.js
@@ -0,0 +1,8 @@
+// Toán tử 3 ngôi
+let number = -10;
+let message = number >= 0 ? "Số dương" : "Số âm";
+console.log(message);
+
+let number2 = 10;
+let message2 = number2 % 2 === 0 ? "Số chẵn" : "Số lẻ";
+console.log(message2);
