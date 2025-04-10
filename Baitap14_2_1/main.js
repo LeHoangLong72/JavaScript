e96d316eee8b9d605b16251ba3ab12ac29d1a9bc
LeHoangLong72 <se182662lehoanglong@gmail.com>
2025-04-10 19:11:28 +0700

    Update

--- Baitap14_2_1/main.js
+++ Baitap14_2_1/main.js
@@ -1,3 +1,13 @@
+/* 
+Bài tập: Tìm x, y khi biết tổng và hiệu của chúng
+
+case test : tong = 14, hieu = 4 => x = 9, y = 5
+case 2    : tong = 8, hieu = 5 => x = 6.5, y = 1.5
+
+x + y = 14
+x - y = 4
+*/
+
 let tong = Number(prompt("Nhập vào tổng 2 số:"));
 let hieu = Number(prompt("Nhập vào hiệu 2 số:"));
 
